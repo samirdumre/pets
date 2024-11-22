3630103d33b1e4a6f4f79b461ad8e1efad6aac39
samirdumre <samirdumre234@gmail.com>
2024-11-22 09:57:37 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Pet from './Pet.jsx'
+import SearchParams from './SearchParams.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </>
+  )
+}
+
+export default App
