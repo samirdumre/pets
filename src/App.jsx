b7e73f213038674b704a74d0c53458d4294da968
samirdumre <samirdumre234@gmail.com>
2024-11-28 17:54:57 +0545

    react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import './App.css'
-import { Link, BrowserRouter, Route, Routes } from "react-router-dom"
-import SearchParams from './SearchParams.jsx'
-import Details from './Details.jsx'
+import "./App.css";
+import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./SearchParams.jsx";
+import Details from "./Details.jsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 function App() {
-
   return (
-      <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>        
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
